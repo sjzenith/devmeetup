fc38054df0504e4d0ea9d62985bc9099db1d69bd
sjzenith <sjzenith0723@gmail.com>
2020-07-06 21:30:37 +0900

    first commit

---
+++ src/main.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import App from './App'
+import * as firebase from 'firebase'
+import router from './router'
+import { store } from './store'
+import DateFilter from './filters/date'
+import AlertCmp from './components/Shared/Alert.vue'
+
+Vue.use(Vuetify)
+Vue.config.productionTip = false
+
+Vue.filter('date', DateFilter)
+Vue.component('app-alert', AlertCmp)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App),
+  created () {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCFYWd6FpR53u4hSPXQSjOYeZNPF1FxG2M',
+      authDomain: 'yt-devmeetup.firebaseapp.com',
+      databaseURL: 'https://yt-devmeetup.firebaseio.com',
+      projectId: 'yt-devmeetup',
+      storageBucket: ''
+    })
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.$store.dispatch('autoSignIn', user)
+      }
+    })
+    this.$store.dispatch('loadMeetups')
+  }
+})
